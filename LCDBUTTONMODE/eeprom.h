ad6daad61a670d5d85e8b2a81a1d61a58ecaa92d
Roman <Romachester2021@gmail.com>
2023-01-29 13:37:23 +0100

    Create eeprom.h

---
+++ LCDBUTTONMODE/eeprom.h
@@ -0,0 +1,14 @@
+﻿
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+#include "main.h"
+
+void EEPROM_write(unsigned int uiAddress, unsigned char ucData);
+unsigned char EEPROM_read(unsigned int uiAddress);
+void EEPROM_write_2bit(unsigned int uiAddress, uint16_t ucData);
+uint16_t EEPROM_read_2bit(unsigned int uiAddress);
+uint32_t EEPROM_read_int(unsigned int uiAddress);
+void EEPROM_write_int(unsigned int uiAddress, uint32_t ucData);
+
+#endif /* EEPROM_H_ */
