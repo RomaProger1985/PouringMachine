363c3d727a941b6694b47cd564d57b5c25b70d4a
Roman <Romachester2021@gmail.com>
2023-01-29 13:37:58 +0100

    Create menu.h

---
+++ LCDBUTTONMODE/menu.h
@@ -0,0 +1,40 @@
+﻿
+#ifndef MENU_H_
+#define MENU_H_
+
+#include "main.h"
+
+typedef enum {
+	MENU_STATE_IDLE = 0,
+	MENU_STATE_WAIT,
+	MENU_STATE_SETING,
+	MENU_STATE_STARTING,
+	MENU_STATE_STARTED,
+	MENU_STATE_MANUAL,
+}MENU_StateTypeDef;
+
+typedef enum {
+	RinghtSeparator_On = 0,
+	LeftSeparator_On,
+	Lift_On,
+	Value_On,
+	Cover_On,
+}Manual_State;
+
+typedef enum {
+	None = 0,
+	BootelSpaising,
+	InBootleDelau,
+	ValyeDelay,
+	liftdownDelay,
+	BottleOutDelay,
+	AfterLiftUpDelay,
+	AfterLiftDownDelay,
+}Set_ParametrsTypeDef;
+
+unsigned char setingNavigation;
+MENU_StateTypeDef menu_state;
+
+void MenuProcess(void);
+
+#endif /* MENU_H_ */
